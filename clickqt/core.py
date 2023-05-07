b6c5234a6c05fafaaf6a8703d53bddad06fa89e2
Timo Wild <timo.wi@aol.com>
2023-05-07 17:52:50 +0200

    Add GUI elements for count, password and choice

--- clickqt/core.py
+++ clickqt/core.py
@@ -1,52 +1,89 @@
 import click
 import inspect
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QCheckBox, QPushButton, QLineEdit, QGroupBox, QLabel, QComboBox
 
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QCheckBox, QPushButton, QLineEdit, QGroupBox
+from clickqt.checkableComboBox import CheckableComboBox
 
 
 def qtgui_from_click(cmd):
     widget_registry = {}
 
     def parameter_to_widget(o):
-        if isinstance(o.type, click.types.BoolParamType):
-            widget = QCheckBox(o.name)
-            if o.default:
-                widget.setChecked(True)
-            widget_registry[o.name] = lambda: widget.isChecked()
-            return widget
-        
-        if isinstance(o.type, click.types.StringParamType):
-            widget = QLineEdit(o.name)
-            widget_registry[o.name] = lambda: widget.text()
-            return widget
+        if o.name: # CLI-Parameter has a name
+            param = QWidget()
+            hBox = QHBoxLayout()
+            widget = None
+            param.setLayout(hBox)
+            label = QLabel(f"{o.name}: ")
+            if hasattr(o, "help"): # Only options have a help argument
+                label.setToolTip(o.help)
+            hBox.addWidget(label)
+         
+            match o.type:
+                case click.types.BoolParamType():
+                    widget = QCheckBox()
+                    if o.default() if callable(o.default) else o.default:
+                        widget.setChecked(True)
+                    widget_registry[o.name] = lambda: widget.isChecked()
+                case click.types.StringParamType():
+                    widget = QLineEdit(o.default() if callable(o.default) else o.default)
+                    if hasattr(o, "hide_input"): # Only options have a hide_input argument
+                        widget.setEchoMode(QLineEdit.EchoMode.Password)
+                    widget_registry[o.name] = lambda: widget.text()
+                case click.types.IntRange():
+                    widget = QLineEdit()
+                    widget_registry[o.name] = lambda: len(widget.text()) if not widget.text().isnumeric() else int(widget.text())
+                case click.types.Choice():
+                    if not o.multiple:
+                        widget = QComboBox()
+                        widget.addItems(o.to_info_dict()["type"]["choices"])
+                        widget_registry[o.name] = lambda: widget.currentText()
+                    else:
+                        widget = CheckableComboBox()
+                        widget.addItems(o.to_info_dict()["type"]["choices"])
+                        widget_registry[o.name] = lambda: widget.currentData()
+                case _:
+                    raise NotImplementedError(o.type)
+            
+            assert widget is not None, "Widget not initialized"
+
+            hBox.addWidget(widget)
+
+            return param
+        else:
+            raise SyntaxError("No parameter name specified")
     
     def grouped_widgets(title, widgets):
-        group = QGroupBox(title)
-        group_elements = QVBoxLayout()
-        group.setLayout(group_elements)
-        for w in widgets:
-            group_elements.addWidget(w)
-        return group
+        if len(widgets):
+            group = QGroupBox(title)
+            group_elements = QVBoxLayout()
+            group.setLayout(group_elements)
+            for w in widgets:
+                group_elements.addWidget(w)
+            return group
+        return None
     
     app = QApplication([])
+    app.setApplicationName("GUI for CLI")
     window = QWidget()
     layout = QVBoxLayout()
     window.setLayout(layout)
 
-    required = grouped_widgets("Required arguments:", [parameter_to_widget(p) for p in cmd.params if isinstance(p, click.core.Argument)])
-    layout.addWidget(required)
+    required = grouped_widgets("Required arguments:", [parameter_to_widget(p) for p in cmd.params if isinstance(p, click.core.Argument) or (isinstance(p, click.core.Option) and p.required)])
+    if required:
+        layout.addWidget(required)
 
-    optional = grouped_widgets("Optional arguments:", [parameter_to_widget(p) for p in cmd.params if isinstance(p, click.core.Option)])
-    layout.addWidget(optional)
+    optional = grouped_widgets("Optional arguments:", [parameter_to_widget(p) for p in cmd.params if isinstance(p, click.core.Option) and not p.required])
+    if optional:
+        layout.addWidget(optional)
 
-    run_button = QPushButton("&Run")
+    run_button = QPushButton("&Run")    #Shortcut Alt+R
     def run():
         cmd.callback(*tuple(widget_registry[a]() for a in inspect.getfullargspec(cmd.callback).args))
+        app.quit()  #Close GUI
 
     run_button.clicked.connect(run)
 
-    layout.addWidget(required)
-    layout.addWidget(optional)
     layout.addWidget(run_button)
 
     def run_app():
--- example/example/__main__.py
+++ example/example/__main__.py
@@ -1,21 +1,38 @@
 import click
 import clickqt
 
+import os
+
 
 @click.command()
 @click.option(
     "--verbose",
     type=bool,
     is_flag=True,
+    default=True,
+    required=True,
     help="Whether we want verbose output"
 )
-@click.argument("filename")
-def main(verbose, filename):
-    if verbose:
-        click.echo(f"Work on file '{filename}'")
+@click.argument("username", 
+                default=lambda: os.environ.get("USERNAME", ""))
+@click.option('-c', '--count', 
+              count=True, 
+              help="Repetition of the option")
+@click.option('--hash-type-single', 
+              type=click.Choice(['MD5', 'SHA1']))
+@click.option('--hash-type-multiple', 
+              type=click.Choice(['MD5', 'SHA1']), 
+              multiple=True)
+@click.password_option()
+def main(verbose, username, count, hash_type_single, hash_type_multiple, password):
+    click.echo(f"\nverbose: '{verbose}'\n" +
+                f"username: '{username}'\n" +
+                f"count: '{count}'\n" +
+                f"hash_type_single: '{hash_type_single}'\n" +
+                f"hash_type_multiple: '{hash_type_multiple}'\n" +
+                f"password: '{password}'\n") #Only for testing!!!
 
-
-gui = clickqt.qtgui_from_click(main)
 
 if __name__ == "__main__":
+    gui = clickqt.qtgui_from_click(main)
     gui()
