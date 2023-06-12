73ac5c3df9e5e62514a353e08ecf473067253a0a
ErfinderR <timo.wi@aol.com>
2023-06-12 21:20:06 +0200

    Rename "valid_error_message" to "click_error_message"

--- clickqt/core/error.py
+++ clickqt/core/error.py
@@ -8,17 +8,17 @@ class ClickQtError():
         CALLBACK_VALIDATION_ERROR = 3
         CONVERSION_ERROR = 4
 
-    def __init__(self, type: ErrorType=ErrorType.NO_ERROR, trigger: str="", valid_error_message: str=""):
+    def __init__(self, type: ErrorType=ErrorType.NO_ERROR, trigger: str="", click_error_message: str=""):
          self.type = type
          self.trigger = trigger
-         self.valid_error_message = valid_error_message
+         self.click_error_message = click_error_message
 
     def message(self) -> str:
         match(self.type.value):
             case ClickQtError.ErrorType.NO_ERROR: return ""
             case ClickQtError.ErrorType.CONFIRMATION_INPUT_NOT_EQUAL_ERROR: return f"Confirmation input ({self.trigger}) is not equal"
             case ClickQtError.ErrorType.ABORTED_ERROR: return "Aborted"
-            case ClickQtError.ErrorType.CALLBACK_VALIDATION_ERROR: return f"Callback validation error ({self.trigger}): {self.valid_error_message}"
-            case ClickQtError.ErrorType.CONVERSION_ERROR: return f"Convertion error ({self.trigger}): {self.valid_error_message}"
+            case ClickQtError.ErrorType.CALLBACK_VALIDATION_ERROR: return f"Callback validation error ({self.trigger}): {self.click_error_message}"
+            case ClickQtError.ErrorType.CONVERSION_ERROR: return f"Convertion error ({self.trigger}): {self.click_error_message}"
        
         return "Unknown"
