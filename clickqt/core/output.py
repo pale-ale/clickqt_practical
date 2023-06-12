676ff1a5c5da1c366109028c8c9aa9cefcb8fdfd
ErfinderR <timo.wi@aol.com>
2023-06-12 16:41:32 +0200

    Remove terminal output and escape html chars

--- clickqt/core/output.py
+++ clickqt/core/output.py
@@ -2,6 +2,7 @@ import sys
 from io import BytesIO, TextIOWrapper 
 from PySide6.QtWidgets import QPlainTextEdit, QMenu
 from PySide6.QtGui import QTextCursor, QColor, QContextMenuEvent, QAction
+import html
 
 class OutputStream(TextIOWrapper):
         """
@@ -11,12 +12,10 @@ class OutputStream(TextIOWrapper):
             super().__init__(BytesIO(), sys.stdout.encoding)
             self.output = output
             self.color = color
-            self.stdout = sys.stdout
 
         def write(self, message: bytes|str):
             if message:
-                message = message.decode(sys.stdout.encoding) if isinstance(message, bytes) else message  
-                self.stdout.write(message)
+                message = html.escape(message.decode(sys.stdout.encoding) if isinstance(message, bytes) else message) 
                 message = message.replace("\r\n", "\n").replace("\n", "<br>") # Replace '\n' with HTML code
                 self.output.moveCursor(QTextCursor.End)
                 self.output.textCursor().insertHtml(f"<p span style='color: rgb({self.color.red()}, {self.color.green()}, {self.color.blue()})'>{message}</p>")
