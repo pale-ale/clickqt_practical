c6045400385cd5c3acc528bd193c8164d47d453d
ErfinderR <60707176+ErfinderR@users.noreply.github.com>
2023-06-17 20:35:04 +0200

    Comment out tests

--- tests/test_clickqt.py
+++ tests/test_clickqt.py
@@ -1,5 +1,5 @@
-from tests.widget.checkbox import test_checkbox
-from tests.widget.numericfields import test_intfield
+#from tests.widget.checkbox import test_checkbox
+#from tests.widget.numericfields import test_intfield
 
 def test_clickqt():
     assert True
