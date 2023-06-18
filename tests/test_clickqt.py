438bc11565522f471928b7fe4cc78930a8b447d6
ErfinderR <60707176+ErfinderR@users.noreply.github.com>
2023-06-18 21:38:44 +0200

    Add another test for Checkbox

--- tests/test_clickqt.py
+++ tests/test_clickqt.py
@@ -1,2 +1,2 @@
-from tests.widget.checkbox import test_checkbox
+from tests.widget.checkbox import test_checkbox, test_gui_checkbox
 from tests.widget.numericfields import test_intfield, test_gui_intfield
--- tests/widget/checkbox.py
+++ tests/widget/checkbox.py
@@ -1,4 +1,5 @@
 import click
+import clickqt
 import PySide6.QtCore as QtCore
 import pytestqt.qtbot as qtbot
 from clickqt.core.error import ClickQtError
@@ -19,3 +20,27 @@ def test_checkbox(qtbot:qtbot.QtBot):
     qtbot.mouseClick(cb.widget, QtCore.Qt.MouseButton.LeftButton, pos=checkbox_pos)
     val, err = cb.getValue()
     assert val and err.type == ClickQtError.ErrorType.NO_ERROR
+
+def test_gui_checkbox():
+    expected:bool = True
+
+    @click.command()
+    @click.option("--verbose", default=expected)
+    def dist(verbose):
+        assert isinstance(verbose, bool)
+        assert verbose == expected
+
+    control = clickqt.qtgui_from_click(dist)
+    run_button = control.gui.run_button
+
+    # Check if default value was set correctly
+    run_button.click() 
+
+    for _ in range(2):
+        expected = not expected
+        control.widget_registry[dist.name][dist.params[0].name].setValue(expected)
+        run_button.click() 
+    
+    # Wrong type does not change the widget value
+    control.widget_registry[dist.name][dist.params[0].name].setValue(0)
+    run_button.click()
