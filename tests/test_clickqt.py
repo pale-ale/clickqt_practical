a6bb68e9af6c0bfe2b61788c9fdce724165aa34a
ErfinderR <60707176+ErfinderR@users.noreply.github.com>
2023-06-20 13:41:10 +0200

    Add tests for textfield

--- tests/test_clickqt.py
+++ tests/test_clickqt.py
@@ -1,2 +1,3 @@
 from tests.widget.checkbox import test_checkbox, test_gui_checkbox
 from tests.widget.numericfields import test_intfield, test_gui_intfield
+from tests.widget.textfield import test_gui_textfield, test_gui_textfield_envvar
