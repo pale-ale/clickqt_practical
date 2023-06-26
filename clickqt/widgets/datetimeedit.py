f8aea7ab2af409c1b980a4595060a87f76db7910
ErfinderR <60707176+ErfinderR@users.noreply.github.com>
2023-06-26 15:43:45 +0200

    Improve DateTimeEdit implementation

--- clickqt/widgets/datetimeedit.py
+++ clickqt/widgets/datetimeedit.py
@@ -1,7 +1,7 @@
 from PySide6.QtWidgets import QDateTimeEdit
 from PySide6.QtCore import QDateTime
 from clickqt.widgets.basewidget import BaseWidget
-from click import Parameter, ParamType
+from click import Parameter, ParamType, Context
 from typing import Any
 
 
@@ -11,14 +11,16 @@ class DateTimeEdit(BaseWidget):
     def __init__(self, otype:ParamType, param:Parameter, *args, **kwargs):
         super().__init__(otype, param, *args, **kwargs)
 
+        self.widget.setDisplayFormat("dd.MM.yyyy hh:mm:ss")
+
         if self.parent_widget is None and (default := BaseWidget.getParamDefault(param, None)) is not None:
             self.setValue(default)
 
-    def setValue(self, value: str):
-        if isinstance(value, str):
-            self.widget.setDateTime(QDateTime.fromString(value, "yyyy-MM-dd hh:mm:ss"))
+    def setValue(self, value:Any):
+        self.widget.setDateTime(QDateTime.fromString(str(self.type.convert(str(value), self.click_command, Context(self.click_command))), "yyyy-MM-dd hh:mm:ss"))
     
     def getWidgetValue(self) -> str:
-         # click.DateTime wants a str in form of '%Y-%m-%d', '%Y-%m-%dT%H:%M:%S'or '%Y-%m-%d %H:%M:%S'
-         # See https://click.palletsprojects.com/en/8.1.x/api/#click.DateTime
-        return self.widget.dateTime().toString("yyyy-MM-dd hh:mm:ss")
+        # click.DateTime wants a str in form of '%Y-%m-%d', '%Y-%m-%dT%H:%M:%S', '%Y-%m-%d %H:%M:%S' or user defined format
+        # See https://click.palletsprojects.com/en/8.1.x/api/#click.DateTime
+        return self.widget.dateTime().toPython().strftime(self.type.formats[-1]) # Convert the string into the last added format in formats-list (default: %Y-%m-%d %H:%M:%S)
+
