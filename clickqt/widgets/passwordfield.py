8e051293f4670a64d8152a1b3cd70b831135cbfc
ErfinderR <60707176+ErfinderR@users.noreply.github.com>
2023-07-03 09:55:31 +0200

    Improve showPassword function

--- clickqt/widgets/passwordfield.py
+++ clickqt/widgets/passwordfield.py
@@ -18,8 +18,14 @@ class PasswordField(TextField):
         self.show_hide_action.setCheckable(True)
 
         def showPassword(show):
-            self.widget.setEchoMode(QLineEdit.EchoMode.Normal if show else QLineEdit.EchoMode.Password)
-            self.show_hide_action.setIcon(QIcon('clickqt\\images\\eye-hide.png') if show else QIcon('clickqt\\images\\eye-show.png'))
+            if show:
+                self.widget.setEchoMode(QLineEdit.EchoMode.Normal)
+                self.show_hide_action.setIcon(QIcon('clickqt\\images\\eye-hide.png'))
+                self.show_hide_action.setText('Hide password')
+            else:
+                self.widget.setEchoMode(QLineEdit.EchoMode.Password)
+                self.show_hide_action.setIcon(QIcon('clickqt\\images\\eye-show.png'))
+                self.show_hide_action.setText('Show password')
 
         self.show_hide_action.toggled.connect(showPassword)
 
