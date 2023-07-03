43e8e934f369d6c57d17740b49584b93259723d9
ErfinderR <60707176+ErfinderR@users.noreply.github.com>
2023-07-03 09:48:15 +0200

    Refactor and add run_button.click() tests

--- tests/test_execution.py
+++ tests/test_execution.py
@@ -10,6 +10,43 @@ from clickqt.core.error import ClickQtError
 from typing import Any
 
 
+def prepare_execution(monkeypatch:MonkeyPatch, value:Any, widget:clickqt.widgets.BaseWidget, param:click.Parameter) -> tuple[str, str|None]:
+    if isinstance(widget, clickqt.widgets.MessageBox):
+        # Mock the QMessageBox.information-function
+        # User clicked on button "Yes" or "No"
+        monkeypatch.setattr(QMessageBox, "information", lambda *args: QMessageBox.Yes if value else QMessageBox.No)
+    elif isinstance(widget, clickqt.widgets.FileField) and value == "-" and "r" in param.type.mode:
+        monkeypatch.setattr(QInputDialog, "getMultiLineText", lambda *args: (value, True)) # value, ok
+
+    args:str = ""
+
+    if param.multiple:
+        def reduce(value) -> str:
+            arg_str = "--p="
+            for v in value:
+                if isinstance(v, list):
+                    return reduce(v)
+                else:
+                    arg_str += str(v) + " "
+            return arg_str
+        for v in value:
+            if isinstance(v, list):
+                args += reduce(v)
+            else:
+                args += f"--p={str(v)} "
+    elif isinstance(widget, clickqt.widgets.MultiWidget):
+        args = "--p="
+        for v in value:
+            args += str(v) + " "
+    elif not isinstance(widget, clickqt.widgets.MessageBox):
+        args = f"--p={str(value)}"
+
+    input = None if not isinstance(widget, clickqt.widgets.MessageBox) else \
+            "y" if value else "n"
+    
+    return (args, input)
+
+
 @pytest.mark.parametrize(
     ("click_attrs", "value"),
     [
@@ -38,46 +75,23 @@ from typing import Any
     ]
 )
 def test_execution(monkeypatch:MonkeyPatch, runner:CliRunner, click_attrs:dict, value:Any):
+    clickqt_res:Any = None
+
+    def callback(p):
+        nonlocal clickqt_res
+        if clickqt_res is None:
+            clickqt_res = p
+        return p
+
     param = click.Option(param_decls=["--p"], **click_attrs)
-    cli = click.Command("cli", params=[param], callback=lambda p: p)
+    cli = click.Command("cli", params=[param], callback=callback)
     
     control = clickqt.qtgui_from_click(cli)
     widget = control.widget_registry[cli.name][param.name]
 
     widget.setValue(value)
 
-    if isinstance(widget, clickqt.widgets.MessageBox):
-        # Mock the QMessageBox.information-function
-        # User clicked on button "Yes" or "No"
-        monkeypatch.setattr(QMessageBox, "information", lambda *args: QMessageBox.Yes if value else QMessageBox.No)
-    elif isinstance(widget, clickqt.widgets.FileField) and value == "-":
-        monkeypatch.setattr(QInputDialog, "getMultiLineText", lambda *args: (value, True)) # value, ok
-
-    args:str = ""
-
-    if param.multiple:
-        def reduce(value) -> str:
-            arg_str = "--p="
-            for v in value:
-                if isinstance(v, list):
-                    return reduce(v)
-                else:
-                    arg_str += str(v) + " "
-            return arg_str
-        for v in value:
-            if isinstance(v, list):
-                args += reduce(v)
-            else:
-                args += f"--p={str(v)} "
-    elif isinstance(widget, clickqt.widgets.MultiWidget):
-        args = "--p="
-        for v in value:
-            args += str(v) + " "
-    elif not isinstance(widget, clickqt.widgets.MessageBox):
-        args = f"--p={str(value)}"
-
-    input = None if not isinstance(widget, clickqt.widgets.MessageBox) else \
-            "y" if value else "n"
+    args, input = prepare_execution(monkeypatch, value, widget, param)
 
     click_res = runner.invoke(cli, args, input, standalone_mode=False)
     val, err = widget.getValue()
@@ -87,11 +101,20 @@ def test_execution(monkeypatch:MonkeyPatch, runner:CliRunner, click_attrs:dict, 
     if callable(val):
         val, err = val()
 
-    if not isinstance(widget, clickqt.widgets.FileField):  
-        assert val == click_res.return_value
-    else: # IOWrapper-objects can't be compared
-        closest_common_ancestor_class = clcoancl(type(val), type(click_res.return_value))
-        assert closest_common_ancestor_class is not None and closest_common_ancestor_class is not object
-    assert err.type == ClickQtError.ErrorType.NO_ERROR    
-    assert click_res.exception is None
+    # First compare the value from 'widget.getValue()' with the click result
+    # then the clickqt result (run_button clicked) with the click result
+    for i in range(2):
+        if not isinstance(widget, clickqt.widgets.FileField):  
+            assert val == click_res.return_value
+        else: # IOWrapper-objects can't be compared
+            closest_common_ancestor_class = clcoancl(type(val), type(click_res.return_value))
+            assert closest_common_ancestor_class is not None and closest_common_ancestor_class is not object
+        
+        if i == 0:
+            assert err.type == ClickQtError.ErrorType.NO_ERROR    
+            assert click_res.exception is None
+
+            clickqt_res = None # Reset the stored click result
+            control.gui.run_button.click()
+            val = clickqt_res
     
