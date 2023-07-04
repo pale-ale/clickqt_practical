138f322ff83444959d98eb76205988b0eb9d834e
ErfinderR <60707176+ErfinderR@users.noreply.github.com>
2023-07-04 11:40:43 +0200

    Refactor showPassword

--- clickqt/widgets/passwordfield.py
+++ clickqt/widgets/passwordfield.py
@@ -11,21 +11,15 @@ class PasswordField(TextField):
 
         assert hasattr(param, "hide_input") and param.hide_input, "'param.hide_input' should be True"
 
+        self.icon_text = ((QIcon('clickqt\\images\\eye-show.png'), 'Show password'), (QIcon('clickqt\\images\\eye-hide.png'), 'Hide password'))
+        self.show_hide_action = QAction(icon=self.icon_text[0][0], text=self.icon_text[0][1])
         self.widget.setEchoMode(QLineEdit.EchoMode.Password)
-
-        self.show_hide_action = QAction(QIcon('clickqt\\images\\eye-show.png'), 'Show password')
         self.widget.addAction(self.show_hide_action, QLineEdit.ActionPosition.TrailingPosition)
         self.show_hide_action.setCheckable(True)
 
         def showPassword(show):
-            if show:
-                self.widget.setEchoMode(QLineEdit.EchoMode.Normal)
-                self.show_hide_action.setIcon(QIcon('clickqt\\images\\eye-hide.png'))
-                self.show_hide_action.setText('Hide password')
-            else:
-                self.widget.setEchoMode(QLineEdit.EchoMode.Password)
-                self.show_hide_action.setIcon(QIcon('clickqt\\images\\eye-show.png'))
-                self.show_hide_action.setText('Show password')
+            self.widget.setEchoMode(QLineEdit.EchoMode.Normal if show else QLineEdit.EchoMode.Password)
+            self.show_hide_action.setIcon(self.icon_text[show][0])
+            self.show_hide_action.setText(self.icon_text[show][1])
 
         self.show_hide_action.toggled.connect(showPassword)
-
