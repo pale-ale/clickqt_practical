d735650aaee2fe57fa6be0a218b4aa35c50bb07d
Anh Tu Duong Nguyen <anhtuduong.nguyen@outlook.de>
2023-07-30 18:25:57 +0200

    finished custom type support via external usage

--- clickqt/__main__.py
+++ clickqt/__main__.py
@@ -7,13 +7,15 @@ import sys
 from importlib import util, metadata
 
 import click
+from clickqt.core.control import Control
 from clickqt.core.core import qtgui_from_click
 
 
 @click.command("clickqtfy")
 @click.argument("entrypoint")
+@click.argument("gui", default=None, required=False)
 @click.argument("funcname", default=None, required=False)
-def clickqtfy(entrypoint, funcname):
+def clickqtfy(entrypoint, gui, funcname):
     """
     Generate a GUI for an entry point or a file + click.command combinaiton.
 
@@ -21,17 +23,31 @@ def clickqtfy(entrypoint, funcname):
     FUNCNAME: Name of the click.command inside the file at ENTRYPOINT.\n
     If FUNCNAME is provided, ENTRYPOINT is interpreted as a file. Otherwise, as an entry point.
     """
-    if funcname:
-        fileparam = click.types.File()
-        fileparam.convert(entrypoint, None, None)
-        control = qtgui_from_click(get_command_from_path(entrypoint, funcname))
-        control.set_ep_or_path(entrypoint)
-        control.set_is_ep(False)
+    if gui:
+        if funcname:
+            fileparam = click.types.File()
+            fileparam.convert(entrypoint, None, None)
+            control = qtgui_from_click(get_command_from_path(entrypoint, funcname))
+            control.set_custom_mappping(get_gui_specs_from_path(entrypoint, gui))
+            control.set_ep_or_path(entrypoint)
+            control.set_is_ep(False)
+        else:
+            control = qtgui_from_click(get_command_from_entrypoint(entrypoint))
+            control.set_custom_mappping(get_gui_specs_from_entrypoint(gui))
+            control.set_ep_or_path(entrypoint)
+            control.set_is_ep(True)
     else:
-        control = qtgui_from_click(get_command_from_entrypoint(entrypoint))
-        control.set_ep_or_path(entrypoint)
-        control.set_is_ep(True)
-    
+        if funcname:
+            fileparam = click.types.File()
+            fileparam.convert(entrypoint, None, None)
+            control = qtgui_from_click(get_command_from_path(entrypoint, funcname))
+            control.set_ep_or_path(entrypoint)
+            control.set_is_ep(False)
+        else:
+            control = qtgui_from_click(get_command_from_entrypoint(entrypoint))
+            control.set_ep_or_path(entrypoint)
+            control.set_is_ep(True)
+
     return control()
 
 
@@ -66,6 +82,22 @@ def get_entrypoints_from_name(epname: str) -> list[metadata.EntryPoint]:
     return candidates
 
 
+def get_gui_specs_from_entrypoint(epname: str):
+    """
+    Returns the click.Command specified by `epname`.
+    If `epname` is not a click.Command, raises `ImportError`.
+    """
+    eps = get_entrypoints_from_name(epname)
+    if len(eps) == 0:
+        raise ImportError(f"No entry point named '{epname}' found.")
+    if len(eps) > 1:
+        concateps = "\n".join([ep.name for ep in eps])
+        raise ImportError(
+            f"No entry point named '{epname}' found. Similar ones:\n{concateps}"
+        )
+    return validate_gui_ep(eps[0].load())
+
+
 def get_command_from_path(eppath: str, epname: str) -> click.Command:
     """
     Returns the entrypoint given by the file path and the function name,
@@ -84,6 +116,24 @@ def get_command_from_path(eppath: str, epname: str) -> click.Command:
     return validate_entrypoint(entrypoint)
 
 
+def get_gui_specs_from_path(eppath: str, epname: str) -> click.Command:
+    """
+    Returns the entrypoint given by the file path and the function name,
+    or raises `ImportError` if the endpoint is not a `click.Command`.
+    """
+    modulename = "clickqtfy.imported_module"
+    spec = util.spec_from_file_location(modulename, eppath)
+    module = util.module_from_spec(spec)
+    sys.modules[modulename] = module
+    spec.loader.exec_module(module)
+    entrypoint = getattr(module, epname, None)
+    if entrypoint is None:
+        raise ImportError(
+            f"Module '{spec.origin}' does not contain the entry point '{epname}'."
+        )
+    return validate_gui_ep(entrypoint)
+
+
 def validate_entrypoint(entrypoint):
     """
     Raise a `TypeError` if a provided function is not a `click.Command`.
@@ -91,3 +141,9 @@ def validate_entrypoint(entrypoint):
     if not isinstance(entrypoint, click.Command):
         raise TypeError(f"Entry point '{entrypoint}' is not a 'click.Command'.")
     return entrypoint
+
+
+def validate_gui_ep(entrypoint):
+    if not isinstance(entrypoint, Control):
+        raise TypeError(f"Entry point '{entrypoint}' is not a Control")
+    return entrypoint.custom_mapping
