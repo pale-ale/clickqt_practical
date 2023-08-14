023fec65e2094b9d342481c2da5f150c7eb6c1b4
Anh Tu Duong Nguyen <anhtuduong.nguyen@outlook.de>
2023-08-14 16:08:38 +0200

    add concept work

---
+++ tests/test_option_group_widget.py
@@ -0,0 +1,12 @@
+import pytest
+import click
+from tests.dummy_cli import cli
+from clickqt.core.core import qtgui_from_click
+
+
+def test_option_group():
+    pass    
+    
+
+if __name__ == "__main__":
+    test_option_group()
