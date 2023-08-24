d272748ae4cfd3225837886c151c6f1633826931
Tu2-prog <anhtuduong.nguyen@outlook.de>
2023-08-24 12:21:17 +0200

    integrated help text into title widget

--- clickqt/widgets/optiongrouptitlewidget.py
+++ clickqt/widgets/optiongrouptitlewidget.py
@@ -8,7 +8,13 @@ from clickqt.widgets.basewidget import BaseWidget
 class OptionGroupTitleWidget(BaseWidget):
     widget_type = QLabel
 
-    def __init__(self, otype: click.ParamType, param: click.Parameter, **kwargs):
+    def __init__(
+        self,
+        otype: click.ParamType,
+        param: click.Parameter,
+        parent: t.Optional["BaseWidget"] = None,
+        **kwargs,
+    ):
         super().__init__(otype, param, **kwargs)
         self.widget_name = param._GroupTitleFakeOption__group.__dict__["_name"]
         self.label = QLabel(f"<b>{self.widget_name}</b>")
@@ -17,6 +23,14 @@ class OptionGroupTitleWidget(BaseWidget):
         for i in range(self.layout.count()):
             self.layout.itemAt(i).widget().close()
         self.layout.addWidget(self.label)
+        if (
+            isinstance(param, click.Option)
+            and param.help
+            and (parent is None or kwargs.get("vboxlayout"))
+        ):  # Help text
+            self.help_label = QLabel(text=param.help)
+            self.help_label.setWordWrap(True)  # Multi-line
+            self.layout.addWidget(self.help_label)
         self.layout.addWidget(self.line)
 
     def get_widget_value(self) -> t.Any:
