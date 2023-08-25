705c587c919343b3c02d2b02227e8c8da84cbe52
Tu2-prog <anhtuduong.nguyen@outlook.de>
2023-08-25 10:14:48 +0200

    finished correctly working toggle button

--- clickqt/widgets/optiongrouptitlewidget.py
+++ clickqt/widgets/optiongrouptitlewidget.py
@@ -2,10 +2,7 @@ from __future__ import annotations
 import typing as t
 import click
 from PySide6.QtCore import (
-    QPropertyAnimation,
     Qt,
-    QParallelAnimationGroup,
-    QAbstractAnimation,
 )
 from PySide6.QtWidgets import QLabel, QFrame, QToolButton
 from clickqt.widgets.basewidget import BaseWidget
@@ -27,8 +24,9 @@ class OptionGroupTitleWidget(BaseWidget):
         self.line = QFrame()
         self.line.setFrameShape(QFrame.Shape.HLine)
 
-        self.toggle_button = QToolButton(text="Toggle", checkable=True, checked=False)
-        self.toggle_button.setStyleSheet("QToolButton { border: none; }")
+        self.toggle_button = QToolButton(text="Toggle", checkable=True)
+        self.toggle_button_is_checked = self.toggle_button.isChecked()
+        self.toggle_button.setStyleSheet("QToolButton { border: 2px solid; }")
         self.toggle_button.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
         self.toggle_button.setArrowType(Qt.RightArrow)
 
@@ -36,6 +34,7 @@ class OptionGroupTitleWidget(BaseWidget):
 
         for i in range(self.layout.count()):
             self.layout.itemAt(i).widget().close()
+        self.layout.addWidget(self.toggle_button)
         self.layout.addWidget(self.label)
         if (
             isinstance(param, click.Option)
@@ -45,7 +44,6 @@ class OptionGroupTitleWidget(BaseWidget):
             self.help_label = QLabel(text=param.help)
             self.help_label.setWordWrap(True)  # Multi-line
             self.layout.addWidget(self.help_label)
-        self.layout.addWidget(self.toggle_button)
         self.layout.addWidget(self.line)
 
     def get_widget_value(self) -> t.Any:
@@ -59,22 +57,17 @@ class OptionGroupTitleWidget(BaseWidget):
 
     def toggle_option_group(self):
         # Get the current state of the button before toggling
-        print(f"State of the button itself: {self.toggle_button.isChecked()}")
-        current_checked_state = self.toggle_button.isChecked()
-        print(f"Current_checked_state: {current_checked_state}")
-        # Toggle the button state
-        self.toggle_button.setChecked(not current_checked_state)
-        print(f"State of button itself after: {self.toggle_button.isChecked()}")
+        current_checked_state = self.toggle_button_is_checked
 
         # Update the arrow type based on the new state
-        if self.toggle_button.isChecked():
+        if not current_checked_state:
             target_arrow_type = Qt.DownArrow
         else:
             target_arrow_type = Qt.RightArrow
 
         self.update_arrow_icon(target_arrow_type)
-
-        click.echo("Toggleable")
+        # Toggle the button state
+        self.toggle_button_is_checked = not current_checked_state
 
     def update_arrow_icon(self, arrow_type):
         self.toggle_button.setArrowType(arrow_type)
