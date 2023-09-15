bdb0b6004367671e07e0ae6e474a383922f3e0e8
Tu2-prog <anhtuduong.nguyen@outlook.de>
2023-09-15 18:44:30 +0200

    add tests for opt group disable enable

--- tests/test_disable.py
+++ tests/test_disable.py
@@ -1,5 +1,6 @@
 import click
 import pytest
+from click_option_group import OptionGroup
 from clickqt import qtgui_from_click
 from clickqt.core.control import Control
 from tests.testutils import ClickAttrs
@@ -51,6 +52,64 @@ def test_disable(attrs, value, template):
 
     expected_enabled = "--standard 'te st' " + template
     expected_disabled = "--standard 'te st'"
+
+    ensure_cmdstr(control, expected_enabled)
+    widget.set_enabled(False)
+    ensure_cmdstr(control, expected_disabled)
+    widget.set_enabled(True)
+    ensure_cmdstr(control, expected_enabled)
+
+
+@pytest.mark.parametrize(
+    ["attrs", "value", "template"],
+    [
+        (ClickAttrs.checkbox(), "True", "--opt True"),
+        (ClickAttrs.checkbox(is_flag=True), "True", "--opt"),
+        (
+            ClickAttrs.checkable_combobox(["A1", "B2", "C3"]),
+            ["B2", "C3"],
+            "--opt B2 --opt C3",
+        ),
+        (ClickAttrs.filefield(), "/home", "--opt /home"),
+        (ClickAttrs.datetime(), "2001-1-1", "--opt '2001-01-01 00:00:00'"),
+        (ClickAttrs.floatrange(minval=-10), "-2.03", "--opt -2.03"),
+        (ClickAttrs.countwidget(), 3, "--opt --opt --opt"),
+        (
+            ClickAttrs.nvalue_widget(type=(str, int)),
+            [["a", 12], ["b", 11]],
+            "--opt a 12 --opt b 11",
+        ),
+        (
+            ClickAttrs.tuple_widget(types=(str, int, float)),
+            ("t", 1, -2.0),
+            "--opt t 1 -2.0",
+        ),
+        (
+            ClickAttrs.multi_value_widget(nargs=2),
+            ["A", "C"],
+            "--opt A C",
+        ),
+    ],
+)
+def test_disable_with_opt_groups(attrs, value, template):
+    standard_opt = click.Option(["--standard"], default="te st")
+    opt = click.Option(["--opt"], **attrs, default=value)
+    group = OptionGroup("Server configuration")
+
+    @click.command("main")
+    @group.option("--opt1")
+    def cli(**params):
+        print(params)
+
+    cli.params.append(standard_opt)
+    cli.params.append(opt)
+    cmd = cli
+    control = qtgui_from_click(cmd)
+    widget = control.widget_registry[cmd.name]["opt1"]
+    widget.set_value("test")
+
+    expected_enabled = "--opt1 test --standard 'te st' " + template
+    expected_disabled = "--standard 'te st' " + template
 
     ensure_cmdstr(control, expected_enabled)
     widget.set_enabled(False)
