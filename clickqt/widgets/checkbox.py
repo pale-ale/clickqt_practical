6243ba1abae56f89a828625990bb976960ebd16f
pale-ale <alex.baucke@tutanota.com>
2023-09-29 18:21:29 +0200

    enable/disable for bool options w/ flags

--- clickqt/widgets/checkbox.py
+++ clickqt/widgets/checkbox.py
@@ -24,26 +24,40 @@ class CheckBox(BaseWidget):
 
     def __init__(self, otype: click.ParamType, param: click.Parameter, **kwargs):
         super().__init__(otype, param, **kwargs)
-
+        default = self._cast_bool(self.get_param_default(param, False))
         assert isinstance(
             otype, type(click.BOOL)
         ), f"'otype' must be of type '{type(click.BOOL)}', but is '{type(otype)}'."
-
-        if hasattr(self, "help_label"):
-            self.layout.removeWidget(self.help_label)
-            self.widget.setText(self.help_label.text()) # Set text of checkbox to help text
-
+        is_flag = self.param.to_info_dict().get("is_flag", False)
+        if is_flag:
+            self.widget.hide()
+            self.set_enabled_changeable(default)
+        else:
+            self.widget.setChecked(default)
+            if hasattr(self, "help_label"):
+                self.layout.removeWidget(self.help_label)
+                self.help_label.setText(
+                    self.help_label.text()
+                )  # Set text of checkbox to help text
         if self.parent_widget is None:
-            self.set_value(BaseWidget.get_param_default(param, False))
+            self.set_enabled_changeable(default, changeable=True)
 
-    def set_value(self, value: t.Any):
-        self.widget.setChecked(
-            bool(
-                self.type.convert(
-                    str(value), self.click_command, click.Context(self.click_command)
-                )
+    def _cast_bool(self, value: t.Any):
+        return bool(
+            self.type.convert(
+                str(value), self.click_command, click.Context(self.click_command)
             )
         )
 
+    def set_value(self, value: t.Any):
+        if self.param.to_info_dict().get("is_flag", False):
+            self.set_enabled_changeable(value)
+        else:
+            self.widget.setChecked(self._cast_bool(value))
+
     def get_widget_value(self) -> bool:
-        return self.widget.isChecked()
+        return (
+            self.is_enabled
+            if self.param.to_info_dict().get("is_flag", False)
+            else self.widget.isChecked()
+        )
