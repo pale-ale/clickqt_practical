ab6e7a956d6df7e6a6c16a529867a65cb5365c87
ErfinderR <60707176+ErfinderR@users.noreply.github.com>
2023-10-07 12:45:48 +0200

    store output in a variable

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -478,5 +478,7 @@ def test_gui_exception(exception: Exception, output_expected: str):
     run_button.click()  # Start execution
     wait_process_Events(10)  # Wait for starting the worker
 
+    output = terminal_output.toPlainText()
+
     # Worker thread does not sleep so no need to wait for thread to finish
-    assert output_expected in terminal_output.toPlainText()
+    assert output_expected in output
